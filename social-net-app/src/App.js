95938386a7cfb4ae703c82f4bb7c42f0aa31b531
Lexor <lesha.goloborodko392@gmail.com>
2023-11-01 22:02:40 +0200

    first commit

---
+++ social-net-app/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import FormAuth from './pages/FormAuth/FormAuth';
+// import Home from './pages/Home/Home';
+
+function App() {
+  return (
+    <div className="App">
+      {/* <Home /> */}
+      <FormAuth />
+    </div>
+  );
+}
+
+export default App;
